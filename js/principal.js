1ba374f6fd726371ddad2de02dd6a6031f345c9e
Barbara Sousa <barbarapsousa@hotmail.com>
2021-09-02 06:21:53 -0300

    Verificações

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,36 @@
-console.log('Foi carregado de outro mundo!')
-
 var titulo = document.querySelector('.titulo')
 
 titulo.textContent = 'Rosely Nutricionista'
+
+var paciente = document.querySelector('#primeiro-paciente')
+
+var tdPeso = paciente.querySelector('.info-peso')
+var tdAltura = paciente.querySelector('.info-altura')
+var tdImc = paciente.querySelector('.info-imc')
+
+var peso = tdPeso.textContent
+var altura = tdAltura.textContent
+
+var pesoEValido = true
+var alturaEValido = true
+
+if (peso <= 0 || peso >= 1000) {
+  console.log("Peso invalido!");
+  tdPeso.textContent = 'Peso invalido!'
+  pesoEValido = false
+
+}
+
+if (altura <= 0 || altura >= 3.0) {
+  console.log("Peso invalido!");
+  tdAltura.textContent = 'Altura invalida!'
+  alturaEValido = false
+
+}
+
+if (pesoEValido && alturaEValido) {
+  var imc = peso / (altura * altura) // formula do IMC
+  tdImc.textContent = imc
+} else{
+  tdImc.textContent = "Altura e/ou Peso inválidos";
+}
