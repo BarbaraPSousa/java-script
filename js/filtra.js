a8d0fbaa724dec3e55eb87aa446c7c3c35646d10
Barbara Sousa <barbarapsousa@hotmail.com>
2021-09-06 10:49:10 -0300

    Lógica de Filtrar

--- js/filtra.js
+++ js/filtra.js
@@ -1,11 +1,22 @@
-var campoFiltro = document.querySelector("#filtra-tabela") 
+var campoFiltro = document.querySelector('#filtra-tabela')
 
-campoFiltro.addEventListener("input", function() {
- var pacientes = document.querySelectorAll((".paciente"))
- for (var i =0;  i < pacientes.length; i++){
-   var paciente = pacientes[i];
-   var tdNome= paciente.querySelector(".info-nome")
-   var nome = paciente.textContent;
-   console.log(nome);
+campoFiltro.addEventListener('input', function () {
+  var pacientes = document.querySelectorAll('.paciente')
+  if (this.value.length > 0) {
+    for (var i = 0; i < pacientes.length; i++) { // esconde a lista no filtro
+      var paciente = pacientes[i]
+      var tdNome = paciente.querySelector('.info-nome')
+      var nome = tdNome.textContent
+      if (nome != this.value) {
+        paciente.classList.add('invisivel')
+      } else {
+        paciente.classList.remove('invisivel')
+      }
+    }
+  } else {
+    for (var i = 0; i < pacientes.length; i++) { //devolver a lista
+      var paciente = pacientes[i]
+      paciente.classList.remove('invisivel')
+    }
   }
-});
+})
